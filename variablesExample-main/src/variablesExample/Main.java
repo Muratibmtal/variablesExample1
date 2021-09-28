2be7dca0cf4b922348bb88c2daac7509174d730e
Muratibmtal <muratibmtal@gmail.com>
2021-09-28 23:40:40 +0300

    Initial commit

---
+++ variablesExample-main/src/variablesExample/Main.java
@@ -0,0 +1,53 @@
+package variablesExample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String phoneName="Huawei P30 lite";
+        double phonePrice=3200.00;
+        double phoneStar=4.7;
+        boolean phoneIsDiscound=true;
+        int phoneDiscound=7;
+        String processor="2,2 GHz Quad Core + 1,7 GHz Quad Core";
+        String phoneColor="Black";
+        String cameraFeatures="24 MP + 8 MP + 2 MP";
+        String chargingFeature="USB Type-C/Hýzlý Þarj 18W";
+        String numberofCameras="four";
+        String screenResolution="1080 x 2312";
+        String screenType="IPS LCD";
+        String weight="159g";
+        String dimensions="152.9 x 72.7 x 7.4 mm";
+        String InternalMemory="64GB";
+        String ramCapacity="4GB";
+        String mobileconnectionSpeed="4.5G";
+        String batterypowerRange="3000 - 3499 mAh";
+        String batteryType="Li-Po";
+        String wifi="802.11 a/b/g/n/ac";
+        
+        System.out.println("Telefon adý:"+phoneName);
+        System.out.println("iþlemci:"+processor);
+        System.out.println("Rengi:"+phoneColor);
+        System.out.println("Kamera özellikleri:"+cameraFeatures);
+        System.out.println("Þarj Özellikleri:"+chargingFeature);
+        System.out.println("Kamera sayýsý:"+numberofCameras);
+        System.out.println("Ekran çözünürlüðü:"+screenResolution);
+        System.out.println("Ekran tipi:"+screenType);
+        System.out.println("Aðýrlýk:"+weight);
+        System.out.println("Ölçüleri:"+dimensions);
+        System.out.println("Depolama hafýzasý:"+InternalMemory);
+        System.out.println("Ram kapasitesi:"+ramCapacity);
+        System.out.println("Þebeke servisi:"+mobileconnectionSpeed);
+        System.out.println("Batarya kapasitesi:"+batterypowerRange);
+        System.out.println("Batarya Tipi:"+batteryType);
+        System.out.println("Wifi:"+wifi);
+        System.out.println("Fiyatý:"+phonePrice);
+        System.out.println("Yýldýz oraný:"+phoneStar);
+        System.out.println("Ýndirim varmý:"+phoneIsDiscound);
+        System.out.println("Ýndirim oraný:"+phoneDiscound);
+        
+        
+        
+	}
+
+}
